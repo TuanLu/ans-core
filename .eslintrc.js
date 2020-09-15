a5059c703f5c033df1463992bae41d6d9674bb94
Tuan Lu <tuanld@coccoc.com>
2020-09-15 17:50:56 +0700

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": ["airbnb-base", "plugin:prettier/recommended"],
+    "extends": ["airbnb", "plugin:prettier/recommended", "plugin:node/recommended"],
+    // "extends": ["airbnb-base", "plugin:prettier/recommended"],
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
